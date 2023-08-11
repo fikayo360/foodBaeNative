73e01c4e2908c11cae7cf975e76f4e0e0f708ba9
fikayo <fikayoadele@gmail.com>
2023-08-11 14:27:42 +0100

    auth screens

---
+++ changePassword/changePassword.tsx
@@ -0,0 +1,9 @@
+import styles from "./changePasswordstyles";
+
+import React, { FC } from 'react';
+
+const ChangePassword:FC = () => {
+return (<></>)
+}
+
+export default ChangePassword
---
+++ forgotPassword/forgotpassword.tsx
@@ -0,0 +1,8 @@
+import styles from "./forgotpasswordstyles";
+import React, { FC } from 'react';
+
+const ForgotPassword:FC = () => {
+return (<></>)
+}
+
+export default ForgotPassword
